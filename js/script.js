66b3762c751bf49fb821d0a0c76ee54d1b7e6c91
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-10-07 18:02:59 +0200

    Lint corrections

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
-let display = document.getElementById("display");
+let display = document.getElementById('display')
 
-function show(val) {
-	display.innerText = val;
+function show (val) {
+  display.innerText = val
 }
-function math(val) {
-	if (display.innerText == "Error" || display.innerText == "NaN") {
-		return show("");
-	} 
-	if (display.innerText.length < 10) return display.innerText += val;
+function math (val) {
+  if (display.innerText == 'Error' || display.innerText == 'NaN') {
+    return show('')
+  }
+  if (display.innerText.length < 10) return display.innerText += val
 }
-function equals() {
-	if(!display.innerText) return;
-	display.innerText = display.innerText.replace("x", "*");
-	display.innerText = display.innerText.replace("÷", "/");
-	try {
-		show(eval(display.innerText))
-	}
-	catch (e) {
-		show('Error')
-	}
+function equals () {
+  if (!display.innerText) return
+  display.innerText = display.innerText.replace('x', '*')
+  display.innerText = display.innerText.replace('÷', '/')
+  try {
+    show(eval(display.innerText))
+  } catch (e) {
+    show('Error')
+  }
 }
--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const {app, BrowserWindow} = require('electron');
-const path = require('path');
-  
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
 function createWindow () {
-    let mainWindow = new BrowserWindow({
-        width: 310,
-        height: 435,
-        show: false,
-        maximizable: false,
-        autoHideMenuBar: true,
-        title: 'Calculator',
-        resizable: false,
-        icon: path.join(__dirname,"icon/icon.ico")
-    });
-  
-    mainWindow.loadFile('index.html')
+  let mainWindow = new BrowserWindow({
+    width: 310,
+    height: 435,
+    show: false,
+    maximizable: false,
+    autoHideMenuBar: true,
+    title: 'Calculator',
+    resizable: false,
+    icon: path.join(__dirname, 'icon/icon.ico')
+  })
 
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show();
-    });
+  mainWindow.loadFile('index.html')
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 }
-  
-app.on('ready', createWindow);
+
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
